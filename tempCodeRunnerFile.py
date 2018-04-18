8e1e180724a8c70f6aa2e80bb8a7f2895078a750
cezarygarbacz <cezary@elgrom.eu.org>
2018-04-18 18:40:19 +0200

    koniec lekcji1

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+#a = 2
+#print(a)
+
+#a = 'Tekst'
+
+#print(a)  
+
+b = input('Podaj swoje imie: ')
+c = int(input('Podaj swoj wiek'))
+
+print('Witaj ' + b)
+
+if c < 18:
+    print('Jestes pelnoletni ' + b )
+elif c == 25:
+    print('Zaraz skonczysz studia ' + a)
+   
+
---
+++ wsti.py
@@ -0,0 +1,29 @@
+#a = 2
+#print(a)
+
+#a = 'Tekst'
+
+#print(a)  
+
+#b = input('Podaj swoje imie: ')
+#c = int(input('Podaj swoj wiek'))
+
+#print('Witaj ' + b)
+
+#if c < 18:
+#    print('Jestes pelnoletni ' + b )
+#elif c == 25:
+#    print('Zaraz skonczysz studia ' + b)
+
+#numers = [1,2,3,4,5,6]
+
+#for numer in numers
+
+words = ['WSTI',' ', 'studia']  
+
+for word in words:
+    for char in word:
+        print(char)
+
+
+d = input('Podaj srednice kola')
